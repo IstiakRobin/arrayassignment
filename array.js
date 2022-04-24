c012bb386c607b9135bb69f1110d31d3fea15ce6
istiakrobin <istiakhossain6@gmail.com>
2022-04-24 21:36:42 +0600

    assignment

---
+++ array.js
@@ -0,0 +1,5 @@
+let flowers=['rose','joba','shiuli','hasnahena'];
+let strng= 'Hello sobaike, ki obostha sobar, asha kori valo achen';
+let news= strng.split(',');
+flowers.shift();
+console.log(flowers);
