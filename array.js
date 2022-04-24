dd3a9aa3e6c79ec7090bc87372c75e034b29f543
Istiak Robin <72946671+IstiakRobin@users.noreply.github.com>
2022-04-25 05:23:58 +0600

    Delete array.js

--- array.js
+++
@@ -1,5 +0,0 @@
-let flowers=['rose','joba','shiuli','hasnahena'];
-let strng= 'Hello sobaike, ki obostha sobar, asha kori valo achen';
-let news= strng.split(',');
-flowers.shift();
-console.log(flowers);
